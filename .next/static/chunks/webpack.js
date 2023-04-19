010e1081b4adcc0dd8cf23274a37d92ed42177e2
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-19 18:04:23 -0300

    commented out .m-card border in marketplace.css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f08febcef5ff8662"; }
+/******/ 		__webpack_require__.h = function() { return "fe7070b679ff3838"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
